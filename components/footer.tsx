5fc73ab9ef56c86ca3562aa8edf0ac442b0e6ecb
trace2798 <113078518+trace2798@users.noreply.github.com>
2024-01-19 17:06:19 +0530

    added social link on footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,18 +1,72 @@
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
 
+const socials = [
+  {
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="18"
+        fill="none"
+        viewBox="0 0 1200 1227"
+      >
+        <path
+          fill="#fff"
+          d="M714.163 519.284 1160.89 0h-105.86L667.137 450.887 357.328 0H0l468.492 681.821L0 1226.37h105.866l409.625-476.152 327.181 476.152H1200L714.137 519.284h.026ZM569.165 687.828l-47.468-67.894-377.686-540.24h162.604l304.797 435.991 47.468 67.894 396.2 566.721H892.476L569.165 687.854v-.026Z"
+        />
+      </svg>
+    ),
+    link: "https://twitter.com/crackeddevs",
+  },
+  {
+    svg: (
+      <svg
+        viewBox="0 0 256 199"
+        width="18"
+        height="18"
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="xMidYMid"
+      >
+        <path
+          d="M216.856 16.597A208.502 208.502 0 0 0 164.042 0c-2.275 4.113-4.933 9.645-6.766 14.046-19.692-2.961-39.203-2.961-58.533 0-1.832-4.4-4.55-9.933-6.846-14.046a207.809 207.809 0 0 0-52.855 16.638C5.618 67.147-3.443 116.4 1.087 164.956c22.169 16.555 43.653 26.612 64.775 33.193A161.094 161.094 0 0 0 79.735 175.3a136.413 136.413 0 0 1-21.846-10.632 108.636 108.636 0 0 0 5.356-4.237c42.122 19.702 87.89 19.702 129.51 0a131.66 131.66 0 0 0 5.355 4.237 136.07 136.07 0 0 1-21.886 10.653c4.006 8.02 8.638 15.67 13.873 22.848 21.142-6.58 42.646-16.637 64.815-33.213 5.316-56.288-9.08-105.09-38.056-148.36ZM85.474 135.095c-12.645 0-23.015-11.805-23.015-26.18s10.149-26.2 23.015-26.2c12.867 0 23.236 11.804 23.015 26.2.02 14.375-10.148 26.18-23.015 26.18Zm85.051 0c-12.645 0-23.014-11.805-23.014-26.18s10.148-26.2 23.014-26.2c12.867 0 23.236 11.804 23.015 26.2 0 14.375-10.148 26.18-23.015 26.18Z"
+          fill="#5865F2"
+        />
+      </svg>
+    ),
+    link: "https://discord.com/invite/78eNHH3MVJ",
+  },
+  {
+    svg: (
+      <svg
+        width="18"
+        height="18"
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="xMidYMid"
+        viewBox="0 0 256 256"
+      >
+        <path
+          d="M218.123 218.127h-37.931v-59.403c0-14.165-.253-32.4-19.728-32.4-19.756 0-22.779 15.434-22.779 31.369v60.43h-37.93V95.967h36.413v16.694h.51a39.907 39.907 0 0 1 35.928-19.733c38.445 0 45.533 25.288 45.533 58.186l-.016 67.013ZM56.955 79.27c-12.157.002-22.014-9.852-22.016-22.009-.002-12.157 9.851-22.014 22.008-22.016 12.157-.003 22.014 9.851 22.016 22.008A22.013 22.013 0 0 1 56.955 79.27m18.966 138.858H37.95V95.967h37.97v122.16ZM237.033.018H18.89C8.58-.098.125 8.161-.001 18.471v219.053c.122 10.315 8.576 18.582 18.89 18.474h218.144c10.336.128 18.823-8.139 18.966-18.474V18.454c-.147-10.33-8.635-18.588-18.966-18.453"
+          fill="#0A66C2"
+        />
+      </svg>
+    ),
+    link: "https://www.linkedin.com/company/crackeddevs",
+  },
+];
 export const Footer = () => {
   return (
     <div className="fixed bottom-0 w-full p-4 border-t border-muted ">
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-          <Button size="sm" variant="ghost">
-            Privacy Policy
-          </Button>
-          <Button size="sm" variant="ghost">
-            Terms of Service
-          </Button>
+          {socials.map((social) => (
+            <a href={social.link} target="_blank">
+              <Button size="sm" variant="ghost">
+                {social.svg}
+              </Button>
+            </a>
+          ))}
         </div>
       </div>
     </div>
